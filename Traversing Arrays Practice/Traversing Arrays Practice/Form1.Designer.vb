aa78d031f577b992156c310f0b1183c2a9e80706
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-10-18 10:11:20 -0400

    Add files via upload

---
+++ Traversing Arrays Practice/Traversing Arrays Practice/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(12, 21)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Long to Loop"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(12, 59)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(177, 32)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Print the Bs"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(12, 97)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(177, 32)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Even Indexes"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(12, 135)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(177, 32)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Reverse"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(210, 21)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(177, 154)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(440, 209)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Traversing Array Exercises"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Traversing Arrays Practice/Traversing Arrays Practice/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    'Array of test scores
+    Dim testScores = {56, 90, 89, 72, 43, 99, 85, 96, 87, 93, 83, 84, 70, 79, 88, 97, 90, 80, 82, 91}
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Shorten this traversal with a loop
+        ListBox1.Items.Add(testScores(0))
+        ListBox1.Items.Add(testScores(1))
+        ListBox1.Items.Add(testScores(2))
+        ListBox1.Items.Add(testScores(3))
+        ListBox1.Items.Add(testScores(4))
+        ListBox1.Items.Add(testScores(5))
+        ListBox1.Items.Add(testScores(6))
+        ListBox1.Items.Add(testScores(7))
+        ListBox1.Items.Add(testScores(8))
+        ListBox1.Items.Add(testScores(9))
+        ListBox1.Items.Add(testScores(10))
+        ListBox1.Items.Add(testScores(11))
+        ListBox1.Items.Add(testScores(12))
+        ListBox1.Items.Add(testScores(13))
+        ListBox1.Items.Add(testScores(14))
+        ListBox1.Items.Add(testScores(15))
+        ListBox1.Items.Add(testScores(16))
+        ListBox1.Items.Add(testScores(17))
+        ListBox1.Items.Add(testScores(18))
+        ListBox1.Items.Add(testScores(19))
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Display only the scores between 80 and 89
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Display the elements located at even indexs (0, 2, 4, etc.)
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'Display the elements in reverse order (91, 82, 80... and so on)
+
+
+    End Sub
+End Class
